c2b337d9fd3897403d464f3b2629fc7775ab5b80
rizki wahyudie git <rizkiwahyudie@gmail.com>
2022-02-05 22:03:20 +0700

    add file

---
+++ tugas19.js
@@ -0,0 +1,23 @@
+// TUGAS19-1
+console.log("TUGAS 19-1")
+function repeatt(){
+  var x = "Semangat...";
+  console.log(x.repeat(5));
+}
+
+repeatt();
+
+// TUGAS19-2
+console.log("TUGAS 19-2")
+function repeatt2(){
+  var y = "*";
+  console.log(y.repeat(5));
+  console.log(y.repeat(25));
+  console.log(y.repeat(50));
+  console.log(y.repeat(75));
+  console.log(y.repeat(50));
+  console.log(y.repeat(25));
+  console.log(y.repeat(5));
+}
+
+repeatt2();
